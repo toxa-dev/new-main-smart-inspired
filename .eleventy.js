e286feac8dd616791250d93f0bf499db93eea4ac
Tony <anxa.dev@gmail.com>
2025-04-06 15:46:23 +0300

    ..

--- .eleventy.js
+++ .eleventy.js
@@ -10,7 +10,7 @@ module.exports = function (eleventyConfig) {
   return {
     dir: {
       input: "src",
-      output: "_site",
+      output: "./_site",
       includes: "_includes",
     },
     templateFormats: ["njk", "md", "html"],
